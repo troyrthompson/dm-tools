edb155f8734c01894db70f23295de9c78236847c
troyrthompson <troyrthompson@gmail.com>
2024-09-07 11:49:22 -0700

    Update nav styles

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -11,13 +11,13 @@ export const Nav = () => {
   return (
     //{`${styles.link} ${pathname === "/" ? styles.active : ""}`}
     <nav className="flex justify-center py-2">
-      <Link className="px-2 py-1 text-lg font-bold" href="/">
-        Home
+      <Link className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="/">
+        Overview
       </Link>
-      <Link className="px-2 py-1 text-lg font-bold" href="/characters">
+      <Link className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="/characters">
         Characters
       </Link>
-      <Link className="px-2 py-1 text-lg font-bold" href="/rollers">
+      <Link className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="/rollers">
         Rollers
       </Link>
     </nav>
