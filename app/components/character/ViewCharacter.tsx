1550d1359dc1d44ae520426a257db10c4a3a1870
troyrthompson <troyrthompson@gmail.com>
2024-09-08 16:58:29 -0700

    Cleanup view character

--- app/components/character/ViewCharacter.tsx
+++ app/components/character/ViewCharacter.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import { useRouter } from 'next/navigation'
 import { useSearchParams } from 'next/navigation';
-
-import {
-    selectCharacterById,
-    parseCharacterProperties,
-} from "@/lib/features/characters/charactersSlice";
-
+import { selectCharacterById } from "@/lib/features/characters/charactersSlice";
 import { ViewCharacterSheet } from './ViewCharacterSheet';
-
 import { useAppSelector } from "@/lib/hooks";
-
 
 export const Char = () => {
   const searchParams = useSearchParams();
   const characterId = searchParams.get('id');
 
+  if (!characterId) {
+    return <div>Invalid ID</div>;
+  }
+
   const character = useAppSelector((state) => selectCharacterById(state, parseInt(characterId)));
+
+  if (!character) {
+    return <div>No Character Found</div>;
+  }
 
   return (
     <>
