8aa27ed5b0a704e44fbaa5e5620cbf6b9abed2de
troyrthompson <troyrthompson@gmail.com>
2024-09-07 18:56:02 -0700

    Allow larger names in character card

--- app/components/characters/CharacterCard.tsx
+++ app/components/characters/CharacterCard.tsx
@@ -18,10 +18,10 @@ export const CharacterCard = ({character}) => {
     dispatch(updateCharacter(updatedCharacter));
   }
   return (
-    <div className="sm:w-1/12 w-full min-w-32 block w-full-sm bg-white border border-gray-200 rounded shadow dark:bg-gray-800 dark:border-gray-700 no-underline">
+    <div className="min-w-1/12 min-w-32 block w-full-sm bg-white border border-gray-200 rounded shadow dark:bg-gray-800 dark:border-gray-700 no-underline">
         <div className="flex flex-col items-center pb-3 pt-3">
-        <Link href={`./character/?id=${character.id}`} key={character.id}><h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{character.general.name}</h5></Link> 
-            <span className="text-sm text-gray-500 dark:text-gray-400">{character.general.race} {character.general.class}</span>
+        <Link className="text-center" href={`./character/?id=${character.id}`} key={character.id}><h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white px-4">{character.general.name}</h5>
+            <span className="text-sm text-gray-500 dark:text-gray-400">{character.general.race} {character.general.class}</span></Link> 
             <span className="text-md text-gray-700 dark:text-gray-400">HP: <input className="w-12 border border-gray-200" onChange={handleChange} type="number" value={hitPoints}></input></span>
         </div>
     </div>    
