c928fbd8453ded6ee8a3c1c827ba57341faaa611
troyrthompson <troyrthompson@gmail.com>
2024-09-07 19:36:12 -0700

    Cleanup nav template

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,8 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import styles from "../styles/layout.module.css";
-
 export const Nav = () => {
   const pathname = usePathname();
 
