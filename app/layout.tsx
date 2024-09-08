8eafd82a38e3ddda1b80889863fb8912869e4324
troyrthompson <troyrthompson@gmail.com>
2024-09-07 19:43:46 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { RollList } from "./components/dice/RollList";
 
 import "./styles/globals.css";
 import { Roll } from "./components/dice/Roll";
-import styles from "./styles/layout.module.css";
 
 interface Props {
   readonly children: ReactNode;
@@ -19,13 +18,13 @@ export default function RootLayout({ children }: Props) {
       <html lang="en">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <body>
-            <section className="container mx-auto relative min-h-screen flex content-between justify-between flex-col">
             <aside className="sm:fixed text-center sm:text-left p-3 top-0 left-0 bg-white rounded-xl shadow-lg mb-4">
               <RollList/>
             </aside>
             <aside className="sm:fixed max-w-none px-4 bottom-0 left-0">
-            <Roll />
+              <Roll />
             </aside>
+            <section className="container mx-auto relative min-h-screen flex content-between justify-between flex-col">
             <Nav />
             <main className="max-w-none px-4">{children}</main>
             <footer className="max-w-none px-4"></footer>
