c5ce92640cb4817e8e88e3c804bfb2f126984dfb
troyrthompson <troyrthompson@gmail.com>
2024-09-08 17:00:14 -0700

    Update defaultCharacters.ts

--- lib/defaultCharacters.ts
+++ lib/defaultCharacters.ts
@@ -12,12 +12,12 @@ export const defaultCharacters: CharactersSlice = {
             hitPoints: 20,
         },
         abilityScores: {
-            strength: 10,
-            dexterity: 12,
-            constitution: 14,
-            intelligence: 16,
-            wisdom: 18,
-            charisma: 20
+            strength: 16,
+            dexterity: 14,
+            constitution: 16,
+            intelligence: 8,
+            wisdom: 10,
+            charisma: 10
         },
         skillProficiencies: [
             'Acrobatics',
@@ -25,7 +25,7 @@ export const defaultCharacters: CharactersSlice = {
         ],
         savingThrowProficiencies: [
             'Strength',
-            'Dexterity'
+            'Constitution'
         ]
     },{
         id: 2,
@@ -38,12 +38,12 @@ export const defaultCharacters: CharactersSlice = {
             hitPoints: 18,
         },
         abilityScores: {
-            strength: 10,
-            dexterity: 12,
-            constitution: 14,
-            intelligence: 16,
+            strength: 8,
+            dexterity: 10,
+            constitution: 10,
+            intelligence: 18,
             wisdom: 18,
-            charisma: 20
+            charisma: 12
         },
         skillProficiencies: [
             'Acrobatics',
@@ -52,6 +52,58 @@ export const defaultCharacters: CharactersSlice = {
         savingThrowProficiencies: [
             'Intelligence',
             'Wisdom'
+        ]
+    }, {
+        id: 3,
+        general: {
+            name: 'Smithy',
+            race: 'dwarf',
+            class: 'cleric',
+            level: 3,
+            maxHitPoints: 17,
+            hitPoints: 17,
+        },
+        abilityScores: {
+            strength: 12,
+            dexterity: 8,
+            constitution: 12,
+            intelligence: 12,
+            wisdom: 18,
+            charisma: 10
+        },
+        skillProficiencies: [
+            'Insight',
+            'Animal Handling'
+        ], 
+        savingThrowProficiencies: [
+            'Constitution',
+            'Wisdom'
+        ]
+    }, {
+        id: 4,
+        general: {
+            name: 'Digner',
+            race: 'human',
+            class: 'rogue',
+            level: 2,
+            maxHitPoints: 12,
+            hitPoints: 12,
+        },
+        abilityScores: {
+            strength: 10,
+            dexterity: 18,
+            constitution: 8,
+            intelligence: 10,
+            wisdom: 10,
+            charisma: 14
+        },
+        skillProficiencies: [
+            'Sleight of Hand',
+            'Persuasion'
+        ], 
+        savingThrowProficiencies: [
+            'Dexterity',
+            'Charisma'
         ]
     }],
 };
