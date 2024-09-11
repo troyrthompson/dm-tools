39c1d82e6b3073f1db0678e7c1fc81309932b6c0
troyrthompson <troyrthompson@gmail.com>
2024-09-11 13:43:00 -0700

    Update types in language widget

--- app/components/languages/Languages.tsx
+++ app/components/languages/Languages.tsx
@@ -11,12 +11,16 @@ import { useAppSelector } from "@/lib/hooks";
 import { Widget } from "../elements/Widget";
 import { WidgetSelect } from "../elements/WidgetSelect";
 
+import type { Language } from "@/lib/types/character";
+
+type LanguageOptions = Language | 'Options';
+
 export const Languages = () => {
 
   const characters = useAppSelector(selectCharacters);
 
   const [applicableCharacters, setApplicableCharacters] = useState<Array<string>>([]);
-  const [language, setLanguage] = useState('Options');
+  const [language, setLanguage] = useState<LanguageOptions>('Options');
 
   const characterLanguages = characters.map((character) => {
     return {
@@ -26,11 +30,12 @@ export const Languages = () => {
   });
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLanguage(e.target.value);
-    determineApplicableCharacters(e.target.value);
+    const language = e.target.value as Language;
+    setLanguage(language);
+    determineApplicableCharacters(language);
   };
 
-  function determineApplicableCharacters(language: string) {
+  function determineApplicableCharacters(language: Language) {
     const applicableCharacters: Array<string> = [];
     characterLanguages.forEach((character) => {
       if (character.languages[language]) {
